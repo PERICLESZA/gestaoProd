c2caa2780b5d9cc2ca8dfd3a37031804b07ca463
Pericles <pericles1011@yahoo.com.br>
2019-06-26 17:00:58 -0300

    'ConfigHeroku'

--- gestaoProd/settings.py
+++ gestaoProd/settings.py
@@ -27,10 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
-#['gestao-prd.herokuapp.com']
-
+ALLOWED_HOSTS = ['g-produto.herokuapp.com']
 
 # Application definition
 
