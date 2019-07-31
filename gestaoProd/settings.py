ebcd080622133d4db1f432e0993ae75cf998788f
Pericles Zacarias Abrahao <pericles1011u@yahoo.com.br>
2019-07-31 15:08:06 -0300

    Novaatualizacao

--- gestaoProd/settings.py
+++ gestaoProd/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['g-produto.herokuapp.com']
+ALLOWED_HOSTS = ['g-produto.herokuapp.com', 'localhost']
 
 # Application definition
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'produtos',
     'home',
 ]
@@ -144,4 +145,6 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'produto_list'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = ['statics',]
 
--- produtos/models.py
+++ produtos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Produto(models.Model):
     id = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=50)
+    nome = models.CharField('Nome', max_length=50)
     descricao = models.CharField(max_length=100)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     data = models.DateField()
